edb029394eacaf9d4e5db4913b786360faef059a
jiangcaidu <yiyanwenxin@gmail.com>
2025-12-05 19:57:16 +0800

    代码调整

--- springboot/src/main/java/org/example/springboot/SpringbootApplication.java
+++ springboot/src/main/java/org/example/springboot/SpringbootApplication.java
@@ -2,14 +2,28 @@ package org.example.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
 public class SpringbootApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(SpringbootApplication.class, args);
+        printStartupSuccess();
     }
 
+    private static void printStartupSuccess() {
+        String catFace =
+                " /\\___/\\\n" +
+                        " (='.'=)   🐾 宠物管理系统 🐾\n" +
+                        " (\")_(\")   版本 1.0.0";
+
+        System.out.println("=================================================");
+        System.out.println(catFace);
+        System.out.println("🚀 服务启动成功！！！");
+        System.out.println("⏰ 启动时间: " + new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new java.util.Date()));
+        System.out.println("📚 在线文档: http://localhost:12345/doc.html");
+        System.out.println("=================================================");
+    }
 }
